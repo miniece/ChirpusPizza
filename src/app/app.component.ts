773cd601963bdf55b2ab4cfac0c85eeeda40aa58
miniece <mvelinda@icloud.com>
2022-11-10 11:15:17 -0500

    working lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,42 @@ import { Menu } from 'src/menu';
 	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
+	names: Menu[] = [
+		{ name: 'Pepperoni', category: 'Toppings', price: 2 },
+		{ name: 'Bacon', category: 'Toppings', price: 5 },
+		{ name: 'Pepsi', category: 'Drinks', price: 3 },
+		{ name: 'Sprite', category: 'Drinks', price: 3 },
+		{ name: 'Small', category: 'Party Type', price: 20 },
+		{ name: 'Large', category: 'Party Type', price: 50 }
+	];
+	heading: string = 'Menu';
 	title = 'ChirpusPizza';
+
+	getByCategory(): Menu[] {
+		//To refer to something in the same class in a JS object
+		//You MUST use the this keyword
+		//This rule applieds to both properties and methods defined as part of the class
+		//local vars are still okay
+		this.heading = (<HTMLInputElement>document.getElementById('cat')).value;
+		let output: Menu[] = [];
+		for (let i: number = 0; i < this.names.length; i++) {
+			let m: Menu = this.names[i];
+			if (m.category.includes(this.heading)) {
+				output.push(m);
+			}
+		}
+		this.names = output;
+		return output;
+	}
+	reset(): void {
+		this.heading = 'Menu';
+		this.names = [
+			{ name: 'Pepperoni', category: 'Toppings', price: 2 },
+			{ name: 'Bacon', category: 'Toppings', price: 5 },
+			{ name: 'Pepsi', category: 'Drinks', price: 3 },
+			{ name: 'Sprite', category: 'Drinks', price: 3 },
+			{ name: 'Small', category: 'Party Type', price: 20 },
+			{ name: 'Large', category: 'Party Type', price: 50 }
+		];
+	}
 }
-
-let pepperoni: Menu = { name: 'Pepperoni', category: 'Toppings', price: 3 };
-let bacon: Menu = { name: 'Bacon', category: 'Toppings', price: 5 };
-let pepsi: Menu = { name: 'Pepsi', category: 'Drinks', price: 3 };
-let sprite: Menu = { name: 'Sprite', category: 'Drinks', price: 3 };
-let small: Menu = { name: 'Small', category: 'Party Type', price: 20 };
-let large: Menu = { name: 'Large', category: 'Party Type', price: 50 };
-
-let items: Menu[] = [ pepperoni, bacon, pepsi, sprite, small, large ];
